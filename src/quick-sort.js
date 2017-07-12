b4a3f02c697d28bc4d8f67f25e89226c7d70b2c3
neel1043 <neel1043@gmail.com>
2017-07-12 15:39:44 +0530

    Implemented quick sort

--- src/quick-sort.js
+++ src/quick-sort.js
@@ -1,6 +1,29 @@
 'use strict';
 
 function quickSort(input) {
+if (input instanceof Array) {
+
+} else {
+	throw new Error('Invalid')
+}
+
+if(input.length > 0) {
+    return input
+} else {
+    return 
+}
+
+var unsorted = [9, 8, 7, 10, 6, 100, 5, 4, 99, 3, 2, 1];
+
+if (input==utils.sortarray(unsorted))
+{
+}
+else
+{
+throw unmatched
+
+}
+
 
 };
 module.exports = quickSort;
